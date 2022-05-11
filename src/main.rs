74e4feea64b5fd137fabcdb3efe0a7ee5fe313f1
Gearhartlove <finley@kristoff.codes>
2022-05-11 10:56:26 -0600

    Paddle collisions work!

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use bevy::prelude::{Query, Color, Timer};
 use rand::Rng;
 use std::time::Duration;
 use bevy::core::CoreSystem::Time;
+use bevy::sprite::collide_aabb::collide;
 use crate::KeyCode::P;
 
 const WIDTH_WINDOW: i32 = 600;
 const HEIGHT_WINDOW: i32 = 800;
+const PADDLE_OFFSET: i32 =  50;
 
 fn main() {
     App::new()
@@ -28,6 +30,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_system(timer_change_color)
         .add_system(move_sprite_horizontal)
+        .add_system(pong_collision)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run();
 }
@@ -37,7 +40,21 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    // paddle right
+    commands.spawn_bundle( SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.75, 0.28, 0.65),
+            custom_size: Some(Vec2::new(200., HEIGHT_WINDOW as f32)),
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(-(WIDTH_WINDOW as f32)/2., 0., 0.),
+            ..default()
+        },
+        ..default()
+    })
+        .insert(Paddle);
+    //pong ball
     commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
@@ -46,25 +63,42 @@ fn setup(mut commands: Commands) {
         },
         ..default()
     })
-        .insert(PongBall { direction: Direction::East, speed: 5. });
+        .insert(PongBall::default());
 }
 
 fn move_sprite_horizontal(windows: ResMut<Windows>, mut query: Query<(&mut Transform, &mut PongBall)>) {
     let window = windows.get_primary().unwrap();
     let mut speed = 5.;
     let x_window_bounds = window.width()/2.;
+    let y_window_bounds = window.height()/2.;
 
     for (mut transform, mut pong_ball) in query.iter_mut() {
         if transform.translation.x.abs() > x_window_bounds {
-            pong_ball.flip_direction();
+            pong_ball.flip_horizontal_direction();
+            pong_ball.increase_horizontal_speed();
         }
-        match pong_ball.direction {
+        if transform.translation.y.abs() > y_window_bounds {
+            pong_ball.flip_vertical_direction();
+            pong_ball.increase_vertical_speed();
+        }
+        match pong_ball.horizontal_direction {
             Direction::East => {
-                transform.translation.x += pong_ball.speed;
+                transform.translation.x += pong_ball.horizontal_speed;
             }
             Direction::West => {
-                transform.translation.x -= pong_ball.speed;
+                transform.translation.x -= pong_ball.horizontal_speed;
             }
+            _ => {}
+        }
+        match pong_ball.vertical_direction {
+            Direction::North => {
+                transform.translation.y += pong_ball.vertical_speed;
+            }
+            Direction::South => {
+                transform.translation.y -= pong_ball.vertical_speed;
+            }
+
+            _ => {}
         }
 
         //     match transform.translation.x {
@@ -84,7 +118,7 @@ fn move_sprite_horizontal(windows: ResMut<Windows>, mut query: Query<(&mut Trans
 
 struct ColorTimer(Timer);
 
-fn timer_change_color(mut timer: ResMut<ColorTimer>, mut query: Query<&mut Sprite>) {
+fn timer_change_color(mut timer: ResMut<ColorTimer>, mut query: Query<&mut Sprite, Without<Paddle>>) {
     timer.0.tick(Duration::from_secs_f32(0.01)); //manually tick timer
     // change sprites color
     if timer.0.finished() {
@@ -115,24 +149,91 @@ fn keyboard_input(keys: Res<Input<KeyCode>>, mut query: Query<&mut Sprite>) {
 
 #[derive(Component)]
 struct PongBall {
-    direction: Direction,
-    speed: f32,
+    horizontal_direction: Direction,
+    vertical_direction: Direction,
+    horizontal_speed: f32,
+    vertical_speed: f32,
 }
 
 impl PongBall {
-    fn flip_direction(&mut self) {
-        match self.direction {
+    fn flip_horizontal_direction(&mut self) {
+        match self.horizontal_direction {
             Direction::West => {
-                self.direction = Direction::East
+                self.horizontal_direction = Direction::East
             }
             Direction::East => {
-                self.direction = Direction::West
+                self.horizontal_direction = Direction::West
             }
+            _ => {}
+        }
+    }
+
+    fn flip_vertical_direction(&mut self) {
+        match self.vertical_direction {
+            Direction::North => {
+                self.vertical_direction = Direction::South
+            }
+            Direction::South => {
+                self.vertical_direction = Direction::North
+            }
+            _ => {}
+        }
+    }
+
+    fn increase_horizontal_speed(&mut self) {
+        let mut rng = rand::thread_rng();
+        let n: f32 = rng.gen();
+        self.horizontal_speed += self.horizontal_speed * n;
+    }
+
+    fn increase_vertical_speed(&mut self) {
+        let mut rng = rand::thread_rng();
+        let n: f32 = rng.gen();
+        self.vertical_speed += self.vertical_speed * n;
+    }
+
+}
+
+impl Default for PongBall {
+    fn default() -> Self {
+        Self {
+            horizontal_direction: Direction::West,
+            vertical_direction: Direction::South,
+            horizontal_speed: 1.5,
+            vertical_speed: 1.,
         }
     }
 }
 
 enum Direction {
+    North,
+    South,
     East,
     West
 }
+
+#[derive(Component)]
+struct Paddle;
+
+fn pong_collision(mut ball_query: Query<(&mut Transform, &Sprite, &mut PongBall), Without<Paddle>>,
+                    mut paddle_query: Query<(&Transform, &Sprite), With<Paddle>>) {
+    for (mut pong_transform, sprite, mut pong_ball) in ball_query.iter_mut() {
+        let pong_pos = pong_transform.translation.clone();
+        let pong_size = sprite.custom_size.unwrap().clone();
+        for (paddle, sprite) in paddle_query.iter() {
+            let paddle_pos = paddle.translation.clone();
+            let paddle_size = sprite.custom_size.unwrap().clone();
+            // check for collision between pong ball and paddle
+            if let Some(c) =
+                collide(pong_pos, pong_size, paddle_pos, paddle_size) {
+                println!("{:?}", c);
+                pong_ball.flip_horizontal_direction();
+            }
+        }
+    }
+}
+
+
+
+
+
